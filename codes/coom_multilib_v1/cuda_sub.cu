9b348e82f729c74da9fe082a09b698859fcb1584
eric <fantasy_2003_@hotmail.com>
2021-10-21 18:58:07 +0800

    add coom_multilb_v1

---
+++ codes/coom_multilib_v1/cuda_sub.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include "cuda_sub.h"
+#include <iostream>
+using namespace std;
+
+void GetCudaDeviceCount( int &num_gpus )
+{
+    cudaGetDeviceCount( &num_gpus );
+
+    for ( int i = 0; i < num_gpus; ++ i )
+    {
+        cudaDeviceProp dprop;
+        cudaGetDeviceProperties(&dprop, i);
+        printf("%d: %s\n", i, dprop.name);
+    }
+}
---
+++ codes/coom_multilib_v1/main.cpp
@@ -0,0 +1,49 @@
+#ifdef ENABLE_OPENMP
+#include <omp.h>
+#endif
+
+#include <mpi.h>
+
+#ifdef ENABLE_OPENACC
+#include "myopenacc.h"
+#endif
+
+#ifdef ENABLE_CUDA
+#include "cuda_sub.h"
+#endif
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int numprocs,namelen,rank;
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Get_processor_name(processor_name, &namelen);
+	printf("Hello from %d on %s out of %d\n",(rank+1),processor_name,numprocs);
+	
+	#ifdef ENABLE_CUDA
+	int num_gpus = -1;
+	GetCudaDeviceCount( num_gpus );
+	printf("number of CUDA devices:\t%d\n", num_gpus);
+	#endif
+
+	#ifdef ENABLE_OPENACC
+	test_open_acc();
+	#endif
+	
+	#ifdef ENABLE_OPENMP
+	printf("number of host CPUs:\t%d\n", omp_get_num_procs());	
+	#pragma omp parallel num_threads(4)
+	{
+	    int nt = omp_get_thread_num();
+		printf("Hello OneFLOW CFD: Hybrid MPI+Cuda+OpenACC+OpenMP! Thread = %d on CPU Rank%d on Machine %s\n", nt, rank + 1, processor_name);
+	}
+	#endif
+
+	MPI_Finalize();
+	return 0;
+}
