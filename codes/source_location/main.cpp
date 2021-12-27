7befb1417f0d8ab49636b3675c01d5527e4f6af7
eric <fantasy_2003_@hotmail.com>
2021-12-27 13:03:17 +0800

    add source_location

---
+++ codes/source_location/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string_view>
+#include <source_location>
+ 
+void log(const std::string_view message,
+         const std::source_location location = 
+               std::source_location::current())
+{
+    std::cout << "file: "
+              << location.file_name() << "("
+              << location.line() << ":"
+              << location.column() << ") `"
+              << location.function_name() << "`: "
+              << message << '\n';
+}
+ 
+template <typename T> void fun(T x)
+{
+    log(x);
+}
+ 
+int main(int, char*[])
+{
+    log("Hello world!");
+    fun("Hello C++20!");
+}
