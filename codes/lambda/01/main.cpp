ab85588263e5d64983080a885871bb00ac36b7b8
eric <fantasy_2003_@hotmail.com>
2022-01-10 12:40:04 +0800

    add lambda 01

---
+++ codes/lambda/01/main.cpp
@@ -0,0 +1,223 @@
+#include <functional>
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void example1();
+void example2();
+void example3();
+void example4();
+void example5();
+void example6();
+void example7();
+void example8();
+void example9();
+
+int main(int argc, char *argv[])
+{
+    //example1();
+    //example2();
+    //example3();
+    //example4();
+    //example5();
+    //example6();
+    //example7();
+    //example8();
+    example9();
+
+    return 0;
+}
+
+void example1()
+{
+    using namespace std;
+    // Assign the lambda expression that adds two numbers to an auto variable.
+    auto f1 = [](int x, int y) { return x + y; };
+
+    cout << f1(2, 3) << endl;
+
+    // Assign the same lambda expression to a function object.
+    function<int(int, int)> f2 = [](int x, int y) { return x + y; };
+
+    cout << f2(3, 4) << endl;
+}
+
+void example2()
+{
+    using namespace std;
+
+    int i = 3;
+    int j = 5;
+
+    // The following lambda expression captures i by value and
+    // j by reference.
+    function<int (void)> f = [i, &j] { return i + j; };
+
+    // Change the values of i and j.
+    i = 22;
+    j = 44;
+
+    // Call f and print its result.
+    cout << f() << endl;
+}
+
+void example3()
+{
+    using namespace std;
+
+    int n = [] (int x, int y) { return x + y; }(5, 4);
+    cout << n << endl;
+}
+
+void example4()
+{
+    using namespace std;
+
+    // Create a list of integers with a few initial elements.
+    list<int> numbers;
+    numbers.push_back(13);
+    numbers.push_back(17);
+    numbers.push_back(42);
+    numbers.push_back(46);
+    numbers.push_back(99);
+
+    // Use the find_if function and a lambda expression to find the
+    // first even number in the list.
+    const list<int>::const_iterator result =
+        find_if(numbers.begin(), numbers.end(),[](int n) { return (n % 2) == 0; });
+
+    // Print the result.
+    if (result != numbers.end()) {
+        cout << "The first even number in the list is " << *result << "." << endl;
+    } else {
+        cout << "The list contains no even numbers." << endl;
+    }
+}
+
+void example5()
+{
+    using namespace std;
+    // The following lambda expression contains a nested lambda
+    // expression.
+    int timestwoplusthree = [](int x) { return [](int y) { return y * 2; }(x) + 3; }(5);
+
+    // Print the result.
+    cout << timestwoplusthree << endl;
+}
+
+void example6()
+{
+    using namespace std;
+    // The following code declares a lambda expression that returns
+    // another lambda expression that adds two numbers.
+    // The returned lambda expression captures parameter x by value.
+    auto addtwointegers = [](int x) -> function<int(int)> {
+        return [=](int y) { return x + y; };
+    };
+
+    // The following code declares a lambda expression that takes another
+    // lambda expression as its argument.
+    // The lambda expression applies the argument z to the function f
+    // and multiplies by 2.
+    auto higherorder = [](const function<int(int)>& f, int z) {
+        return f(z) * 2;
+    };
+
+    // Call the lambda expression that is bound to higherorder.
+    auto answer = higherorder(addtwointegers(7), 8);
+
+    // Print the result, which is (7+8)*2.
+    cout << answer << endl;
+}
+
+class Scale
+{
+public:
+    // The constructor.
+    explicit Scale(int scale) : _scale(scale) {}
+
+    // Prints the product of each element in a vector object
+    // and the scale value to the console.
+    void ApplyScale(const vector<int>& v) const
+    {
+        for_each(v.begin(), v.end(), [=](int n) { cout << n * _scale << endl; });
+    }
+
+private:
+    int _scale;
+};
+
+void example7()
+{
+    using namespace std;
+    vector<int> values;
+    values.push_back(1);
+    values.push_back(2);
+    values.push_back(3);
+    values.push_back(4);
+
+    // Create a Scale object that scales elements by 3 and apply
+    // it to the vector object. Does not modify the vector.
+    Scale s(3);
+    s.ApplyScale(values);
+}
+
+
+// Negates each element in the vector object. Assumes signed data type.
+template <typename T>
+void negate_all(vector<T>& v)
+{
+    for_each(v.begin(), v.end(), [](T& n) { n = -n; });
+}
+
+// Prints to the console each element in the vector object.
+template <typename T>
+void print_all(const vector<T>& v)
+{
+    for_each(v.begin(), v.end(), [](const T& n) { cout << n << endl; });
+}
+
+void example8()
+{
+    using namespace std;
+    // Create a vector of signed integers with a few elements.
+    vector<int> v;
+    v.push_back(34);
+    v.push_back(-43);
+    v.push_back(56);
+
+    print_all(v);
+    negate_all(v);
+    cout << "After negate_all():" << endl;
+    print_all(v);
+}
+
+void example9()
+{
+    using namespace std;
+    // Create a vector that contains 3 elements.
+    vector<int> elements(3);
+
+    // Create another vector that contains index values.
+    vector<int> indices(3);
+    indices[0] = 0;
+    indices[1] = -1; // This is not a valid subscript. It will trigger an exception.
+    indices[2] = 2;
+
+    // Use the values from the vector of index values to
+    // fill the elements vector. This example uses a
+    // try/catch block to handle invalid access to the
+    // elements vector.
+    try
+    {
+        for_each(indices.begin(), indices.end(), [&](int index) {
+            elements.at(index) = index;
+            });
+    }
+    catch (const out_of_range& e)
+    {
+        cerr << "Caught '" << e.what() << "'." << endl;
+    };
+}
