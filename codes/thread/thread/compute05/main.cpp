161d945a44b41675d884832195a43fb356c15ee3
eric <fantasy_2003_@hotmail.com>
2022-02-02 09:39:27 +0800

    add compute05

---
+++ codes/thread/thread/compute05/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include <string>
+
+void compute( int nIters )
+{
+    for ( int iter = 0; iter < nIters; ++ iter )
+    {
+        std::cout << " iter = " << iter + 1 << " nIters = " << nIters << std::endl;		
+        std::this_thread::sleep_for( std::chrono::milliseconds(500) );
+    }
+    std::cout << "The computation is finished! "  << std::endl;
+}
+
+void gui( double cfl )
+{
+    std::cout << "The CFL number is " << cfl << std::endl;
+}
+
+void wrapfun()
+{
+    std::thread t1( [&] { compute(10); } );
+    t1.detach();
+}
+
+int main()
+{
+    wrapfun();
+    std::this_thread::sleep_for( std::chrono::milliseconds(1500) );
+    gui( 1.2 );
+    return 0;
+}
