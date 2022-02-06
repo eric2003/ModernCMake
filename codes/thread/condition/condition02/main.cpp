bbbfd08ef288a83fb2172ee75f51d00450be73ad
eric <fantasy_2003_@hotmail.com>
2022-02-07 00:00:21 +0800

    add condition02

--- codes/thread/condition/condition02/main.cpp
+++ codes/thread/condition/condition02/main.cpp
@@ -7,17 +7,23 @@ int main( int argc, char **argv )
 {
     std::condition_variable cv;
     std::mutex mtx;
+    bool ready = false;
+
     std::thread th1([&] {
         std::unique_lock lck(mtx);
-        cv.wait(lck);
+        cv.wait(lck, [&] { return ready; });
 
         std::cout << "th1 is awake" << std::endl;
     });
 
     std::this_thread::sleep_for(std::chrono::milliseconds(400));
 
-    std::cout << "notifying..." << std::endl;
-    cv.notify_one();
+    std::cout << "notifying not ready" << std::endl;
+    cv.notify_one();  // useless now, since ready = false
+
+    ready = true;
+    std::cout << "notifying ready" << std::endl;
+    cv.notify_one();  // awakening th1, since ready = true
 
     th1.join();
     return 0;
