388d65d2a34b6ad240c0209f988f0d3757a9096d
eric <fantasy_2003_@hotmail.com>
2022-02-06 21:12:05 +0800

    add deadlock07

---
+++ codes/thread/deadlock/deadlock07/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+std::recursive_mutex mtx;
+
+void bar()
+{
+    std::cout << "bar begin \n";
+    std::cout << "bar mtx.lock()\n";
+    mtx.lock();
+    std::cout << "bar mtx.unlock()\n";
+    mtx.unlock();
+    std::cout << "bar end \n";
+}
+ 
+void foo()
+{
+    std::cout << "foo begin \n";
+    std::cout << "foo mtx.lock()\n";
+    mtx.lock();
+    std::cout << "foo call bar\n";
+    bar();
+    std::cout << "foo mtx.unlock()\n";
+    mtx.unlock();
+    std::cout << "foo end \n";
+}
+
+int main ( int argc, char **argv )
+{
+    foo();
+    return 0;
+}
