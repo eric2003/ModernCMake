2a724d35514ec050e72ce556bdf514942ba60d0d
eric <fantasy_2003_@hotmail.com>
2022-02-12 14:14:32 +0800

    add tuple01

---
+++ codes/moderncpp/tuple/tuple01/main.cpp
@@ -0,0 +1,27 @@
+// tuple example
+#include <iostream>     // std::cout
+#include <tuple>        // std::tuple, std::get, std::tie, std::ignore
+
+int main ()
+{
+  std::tuple<int,char> foo (10,'x');
+  auto bar = std::make_tuple ("test", 3.1, 14, 'y');
+
+  std::get<2>(bar) = 100;                                    // access element
+
+  int myint; char mychar;
+
+  std::tie (myint, mychar) = foo;                            // unpack elements
+  std::tie (std::ignore, std::ignore, myint, mychar) = bar;  // unpack (with ignore)
+
+  mychar = std::get<3>(bar);
+
+  std::get<0>(foo) = std::get<2>(bar);
+  std::get<1>(foo) = mychar;
+
+  std::cout << "foo contains: ";
+  std::cout << std::get<0>(foo) << ' ';
+  std::cout << std::get<1>(foo) << '\n';
+
+  return 0;
+}
