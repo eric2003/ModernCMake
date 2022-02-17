b01a803ffa367542fec14786977bb57515ab054d
eric <fantasy_2003_@hotmail.com>
2022-02-17 14:12:44 +0800

    add foreach03

---
+++ codes/moderncpp/for_each/for_each03/main.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+ 
+struct Sum
+{
+    void operator()(int n) { sum += n; }
+    int sum{0};
+};
+ 
+int main()
+{
+    std::vector<int> nums{3, 4, 2, 8, 15, 267};
+ 
+    auto print = [](const int& n) { std::cout << " " << n; };
+ 
+    std::cout << "before:";
+    std::for_each(nums.cbegin(), nums.cend(), print);
+    std::cout << '\n';
+ 
+    std::for_each(nums.begin(), nums.end(), [](int &n){ n++; });
+ 
+    // calls Sum::operator() for each number
+    Sum s = std::for_each(nums.begin(), nums.end(), Sum());
+ 
+    std::cout << "after: ";
+    std::for_each(nums.cbegin(), nums.cend(), print);
+    std::cout << '\n';
+    std::cout << "sum: " << s.sum << '\n';
+    return 0;
+}
