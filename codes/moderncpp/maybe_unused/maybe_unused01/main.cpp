2f3637c19e901a0807e434a941023d53f6dc43a9
eric <fantasy_2003_@hotmail.com>
2022-03-09 12:06:28 +0800

    add maybe_unused01

---
+++ codes/moderncpp/maybe_unused/maybe_unused01/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main( int argc, char **argv )
+{
+    auto x = 4;
+    int y = 4;
+    int z;
+    [[maybe_unused]] auto xx = 5;
+ 
+    std::cout << "haha " << std::endl;
+    return 0;
+}
