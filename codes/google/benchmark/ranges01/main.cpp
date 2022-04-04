dd072456ec9ceb38a8d3edb3c1308d880cbb340f
eric <fantasy_2003_@hotmail.com>
2022-04-04 22:39:28 +0800

    add ranges01

---
+++ codes/google/benchmark/ranges01/main.cpp
@@ -0,0 +1,20 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+
+static void BM_VectorPushback(benchmark::State& state) {
+  for (auto _ : state) {
+    std::vector<int> data;
+    state.PauseTiming();
+    for (int i = 0; i < state.range(0); ++i)
+      data.push_back( i );
+    state.ResumeTiming();
+    for (int j = 0; j < state.range(1); ++j)
+      data.push_back( j );
+  }
+}
+
+BENCHMARK(BM_VectorPushback)
+    ->Ranges({{128, 256}, {8, 16}});
+
+// Run the benchmark
+BENCHMARK_MAIN();
