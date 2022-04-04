8e7a9bfcd69f15d36a02b87f86aabc78298ea52f
eric <fantasy_2003_@hotmail.com>
2022-04-04 23:23:20 +0800

    add createrange01

---
+++ codes/google/benchmark/createrange01/main.cpp
@@ -0,0 +1,22 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+
+static void BM_VectorPushback(benchmark::State& state) {
+  for (auto _ : state) {
+    std::vector<int> data;
+    state.PauseTiming();
+    for (int i = 0; i < state.range(0); ++i)
+      data.push_back( i );
+    state.ResumeTiming();
+    for (int j = 0; j < state.range(1); ++j)
+      data.push_back( j );
+  }
+}
+
+BENCHMARK(BM_VectorPushback)
+    ->ArgsProduct({
+      benchmark::CreateRange(8, 128, /*multi=*/2),
+      benchmark::CreateRange(1, 4, /*multi=*/2)
+    });
+// Run the benchmark
+BENCHMARK_MAIN();
