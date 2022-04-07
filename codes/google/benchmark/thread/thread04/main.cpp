75616abd765bce7cf71c41cac95616201a0bce83
eric <fantasy_2003_@hotmail.com>
2022-04-07 20:16:35 +0800

    add timer01

---
+++ codes/google/benchmark/thread/thread04/main.cpp
@@ -0,0 +1,25 @@
+#include <benchmark/benchmark.h>
+#include <vector>
+
+static void BM_MultiThreaded(benchmark::State& state) {
+  if (state.thread_index() == 0) {
+    // Setup code here.
+  }
+  for (auto _ : state) {
+    // Run the test as normal.
+    std::vector<int> v;
+    int nSize = 1024;
+    for ( int i = 0; i < nSize; ++ i )
+    {
+        v.push_back(i);
+    }
+  }
+  if (state.thread_index() == 0) {
+    // Teardown code here.
+  }
+}
+
+BENCHMARK(BM_MultiThreaded)->ThreadRange(1, 8)->UseRealTime();
+BENCHMARK(BM_MultiThreaded)->Range(8, 8<<10)->UseRealTime();
+
+BENCHMARK_MAIN();
